3df1d4bdc0152d5c10f89797abd3ecadbff4eb2d
Abhay Prasad <avirajabhay@gmail.com>
2025-05-14 16:30:52 +0530

    password hidden

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/inventory_billing";
     private static final String USER = "root";
-    private static final String PASSWORD = "Mysql@25";
+    private static final String PASSWORD = "****";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
